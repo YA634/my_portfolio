6e3b14e1aebe7170e707beec55a523d483776272
Akihiro Yanagisawa <aki634333year@gmail.com>
2026-07-01 15:03:57 +0900

    add discription 2026/7/1/15:03

--- portfolio.js
+++ portfolio.js
@@ -25,27 +25,37 @@ const datas = {
 	"monster-battle":{
 		title:"monster-battle",
 		img:"img/image copy.png",
-		discription:"バトルロイヤルモードとパーティーバトルモードが遊べます。使用:flask",
-		link:"https://monster-battle-p2v7.onrender.com"
+		discription:
+		"バトルロイヤルモードとパーティーバトルモードが遊べます。<br><br>◯バトルロイヤルモード<br>5匹のモンスターでバトルロイヤル! プレイヤーはモンスターを1匹選んで操作できます。目指すはただ一人の勝者!<br>◯パーティーバトル<br>勇者パーティーとモンスターの集団のガチンコバトル! プレイヤーは勇者パーティーを操作し5匹のモンスターの集団を撃破しよう!<br><br>使用言語: Python3,HTML,CSS<br>使用フレームワーク: flask",
+		link1:"https://monster-battle-p2v7.onrender.com",
+		link2:"https://github.com/YA634/monster_battle"
 	},
 	"cards-app":{
 		title:"cards-app",
 		img:"img/image.png",
-		discription:"バカラ、ブラックジャック、ポーカー(未実装)が遊べます。",
-		link:"https://cards-app-6tfq.onrender.com"
+		discription:"バカラ、ブラックジャック、ポーカー(未実装)が遊べます。<br><br>◯バカラ<br>プレイヤーはバンカーかプレイヤーのどちらが勝つかに賭けます。<br>◯ブラックジャック<br>最強の手札は21! カードを引くも引かないもあなたの選択!<br>◯ポーカー<br>役を揃えて心理戦を挑もう!<br>使用言語: Python3,HTML,CSS<br>使用フレームワーク: Django",
+		link1:"https://cards-app-6tfq.onrender.com",
+		link2:"https://github.com/YA634/cards"
 	}
 };
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 const data = datas[id];
-document.querySelector('.works-title').innerText=data.title
+document.querySelector('h5.works-title').innerText = data.title;
+document.querySelector('h2.works-title').innerText = data.title;
 const imgEl = document.createElement('img');
 imgEl.src = data.img;
 imgEl.alt = data.title;
 document.getElementById('works-img').appendChild(imgEl);
-document.getElementById('works-discription').innerText=data.discription
-const linkEl = document.createElement('a');
-linkEl.href= data.link
-linkEl.textContent = 'アプリを開く';
-linkEl.target = '_blank';
-document.getElementById('works-link').appendChild(linkEl);
+document.getElementById('works-discription').innerHTML=data.discription
+const linkEl1 = document.createElement('a');
+linkEl1.href= data.link1
+linkEl1.textContent = 'アプリを開く';
+linkEl1.target = '_blank';
+const linkEl2 = document.createElement('a');
+linkEl2.href= data.link2
+linkEl2.textContent = 'GitHubのリンク';
+linkEl2.target = '_blank';
+document.getElementById('works-link').appendChild(linkEl1);
+document.getElementById('works-link').appendChild(document.createElement('br'));
+document.getElementById('works-link').appendChild(linkEl2);
